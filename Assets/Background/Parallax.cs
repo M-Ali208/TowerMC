9f25fbb80321041223e3b4443c453a8e4d5e8d63
reqshift <reqshiftt@gmail.com>
2025-01-17 23:14:00 +0300

    Parallax code
    Parallax kodu değiştirildi ama net çalışmıyor.

--- Assets/Background/Parallax.cs
+++ Assets/Background/Parallax.cs
@@ -1,29 +1,31 @@
 using UnityEngine;
-using UnityEngine.UIElements;
+
 public class Parallax : MonoBehaviour
 {
-
     private float lengthx, lengthy, startPosx, startPosy;
-    public GameObject cam;
-    public float parallaxEffect ;
+    public GameObject cam; 
+    public float parallaxEffect;
 
     void Start()
     {
+    
         startPosx = transform.position.x;
-        lengthx  = GetComponent<SpriteRenderer>().bounds.size.x;
         startPosy = transform.position.y;
+
+  
+        lengthx = GetComponent<SpriteRenderer>().bounds.size.x;
         lengthy = GetComponent<SpriteRenderer>().bounds.size.y;
     }
 
     void FixedUpdate()
     {
-        float tempx = (cam.transform.position.x * (1 - parallaxEffect));
-        float distx = (cam.transform.position.x * parallaxEffect);
-        float tempy = (cam.transform.position.y * (1 - parallaxEffect));
-        float disty = (cam.transform.position.y * parallaxEffect);
+     
+        float tempx = cam.transform.position.x * (1 - parallaxEffect);
+        float distx = cam.transform.position.x * parallaxEffect;
+        float tempy = cam.transform.position.y * (1 - parallaxEffect);
+        float disty = cam.transform.position.y * parallaxEffect;
 
-        transform.position = new Vector3(startPosx + distx, transform.position.y, transform.position.z);
-        transform.position = new Vector3(startPosy + disty, transform.position.x, transform.position.z);
+        transform.position = new Vector3(startPosx + distx, startPosy + disty, transform.position.z);
 
         if (tempx > startPosx + lengthx) startPosx += lengthx;
         else if (tempx < startPosx - lengthx) startPosx -= lengthx;
@@ -31,5 +33,4 @@ public class Parallax : MonoBehaviour
         if (tempy > startPosy + lengthy) startPosy += lengthy;
         else if (tempy < startPosy - lengthy) startPosy -= lengthy;
     }
-
 }
