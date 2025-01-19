6a71cff3abf19ac32dd3781a91f62de4e92314e0
reqshift <reqshiftt@gmail.com>
2025-01-19 15:10:03 +0300

    workshop pzr

---
+++ Assets/PlayerController/Duck.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using UnityEngine;
+
+public class Duck : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+    public bool onGround;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private float horizontal;
+    public Vector2Int mousePos;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Ground"))
+            onGround = true;
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Ground"))
+            onGround = false;
+    }
+
+    private void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        float jump = Input.GetAxisRaw("Jump");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        s
+
+        Vector2 movement = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+
+        if (horizontal > 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else if (horizontal < 0)
+            transform.localScale = new Vector3(1, 1, 1);
+
+        if (vertical > 0.1f || jump > 0.1f)
+        {
+            if (onGround)
+                movement.y = jumpForce;
+        }
+        rb.velocity = movement;
+    }
+    private void Update()
+    {
+        mousePos.x = Mathf.RoundToInt(Camera.main.ScreenToWorldPoint(Input.mousePosition).x);
+        mousePos.y = Mathf.RoundToInt(Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+
+        anim.SetFloat("Horizontal", horizontal);
+       
+    }
+
+}
