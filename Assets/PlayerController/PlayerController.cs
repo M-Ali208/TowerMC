3b13c84e9366ab9a277dda4e99d230bc05c0bf95
M-Ali208 <muhammedalituncar@gmail.com>
2025-01-26 00:51:49 +0300

    conflict düzeltmece

--- Assets/PlayerController/PlayerController.cs
+++ Assets/PlayerController/PlayerController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
     public float moveSpeed;
     public float jumpForce;
     //public bool onGround;
@@ -16,80 +17,70 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private LayerMask MouseLayer;
     [SerializeField] private Transform groundCheck;
-    
+
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+
     }
 
     private bool onGrounded()
     {
         return Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, groundLayer);
-        //groundchek player de�il player�n alt�ndaki block�un parent�n�n layer�na bak�lacak
+        //groundchek player değil playerın altındaki blockğun parentının layerına bakılacak
     }
     private void mouseBlockCheck()
     {
-        Physics.Raycast(mousePos,Vector3.back, 1,MouseLayer);
+        Physics.Raycast(mousePos, Vector3.back, 1, MouseLayer);
     }
 
     private void FixedUpdate()
     {
-       horizontal = Input.GetAxis("Horizontal");
+        horizontal = Input.GetAxis("Horizontal");
         float jump = Input.GetAxisRaw("Jump");
         float vertical = Input.GetAxisRaw("Vertical");
 
-        
-        hit= Input.GetMouseButton(0);
+
+        hit = Input.GetMouseButton(0);
         if (hit)
         {
-            
+
         }
 
-    
         Vector2 movement = new Vector2(horizontal * moveSpeed, rb.velocity.y);
 
         if (horizontal > 0)
-            transform.localScale = new Vector3(-1, 1, 1); 
+            transform.localScale = new Vector3(-1, 1, 1);
         else if (horizontal < 0)
-            transform.localScale = new Vector3(1, 1, 1); 
+            transform.localScale = new Vector3(1, 1, 1);
 
-       
-        if (jumpInput > 0.1f && onGround)
+        if (vertical > 0.1f || jump > 0.1f)
         {
             if (onGrounded())
-            movement.y = jumpForce;
-            jump = true;
+                movement.y = jumpForce;
         }
-        else if (onGround)
-        {
-            jump = false;
-        }
-
         rb.velocity = movement;
 
         Debug.Log(Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, groundLayer));
     }
-
     private void Update()
     {
-        
-        anim.SetFloat("Horizontal", Mathf.Abs(horizontal)); 
-        anim.SetBool("hit", Input.GetMouseButton(0));
-        anim.SetBool("jump", jump);
-    }
-}
+        mousePos.x = Mathf.RoundToInt(Camera.main.ScreenToWorldPoint(Input.mousePosition).x);
+        mousePos.y = Mathf.RoundToInt(Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
 
         anim.SetFloat("Horizontal", horizontal);
         anim.SetBool("hit", hit);
-    }  
-    private void OnDrawGizmos() {
+    }
+    private void OnDrawGizmos()
+    {
         Gizmos.color = Color.red;
         Gizmos.DrawLine(groundCheck.position, groundCheck.position + Vector3.down * groundCheckDistance);
     }
     //Debug.Log(gameObjectYouWantTheParentOf.transform.parent.name);
 
- }
-    
- 
+}
+
+
+
