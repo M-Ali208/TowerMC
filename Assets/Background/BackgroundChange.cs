38d6afc1ecc90c91dc6db8471bd86ee3d27b0a61
M-Ali208 <muhammedalituncar@gmail.com>
2025-01-26 20:50:19 +0300

    arka plan rigid body composit col taglar değişikşleri

--- Assets/Background/BackgroundChange.cs
+++ Assets/Background/BackgroundChange.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class BackgroundChange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private SpriteRenderer sr;
+    private float duration = 1f;
+    [SerializeField] private GameObject End;
+    [SerializeField] private GameObject Surface;
+    [SerializeField] private GameObject Underground;
+    [SerializeField] private GameObject Nether;
+    private void Awake()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(fadeEffect());
+            if (gameObject.CompareTag("End"))
+            {
+                End.SetActive(true);
+                Surface.SetActive(false);
+                Underground.SetActive(false);
+                Nether.SetActive(false);
+            }
+            else if (gameObject.CompareTag("Surface"))
+            {
+                End.SetActive(false);
+                Surface.SetActive(true);
+                Underground.SetActive(false);
+                Nether.SetActive(false);
+            }
+            else if (gameObject.CompareTag("Underground"))
+            {
+                End.SetActive(false);
+                Surface.SetActive(false);
+                Underground.SetActive(true);
+                Nether.SetActive(false);
+            }
+            else if (gameObject.CompareTag("Nether"))
+            {
+                End.SetActive(false);
+                Surface.SetActive(false);
+                Underground.SetActive(false);
+                Nether.SetActive(true);
+            }
+        }
+    }
+    private IEnumerator fadeEffect()
+    {
+        float time = 0.0f;
+        while (time < duration)
+        {
+            float alpha = Mathf.Lerp(1, 0, time / duration);
+            Color newColor = sr.color;
+            newColor.a = alpha;
+            sr.color = newColor;
+            time += Time.deltaTime;
+            yield return null;
+        } 
     }
 }
---
+++ Assets/PlayerController/VisibilityManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisibilityManager : MonoBehaviour
+{
+
+    private void OnBecameVisible()
+    {
+        Debug.Log("Visible");
+    }
+
+
+
+
+
+
+
+
+
+}
